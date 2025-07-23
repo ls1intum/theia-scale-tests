e1b2b347f46f013c0fd04c174b612c0e15b4d79e
Tobias Klingenberg <tobikli@pm.me>
2025-07-23 14:08:48 +0200

    Set skip

--- tests/artemis/scorpio.integration.spec.ts
+++ tests/artemis/scorpio.integration.spec.ts
@@ -14,7 +14,7 @@ import path from 'path';
 import { TheiaVSCView } from '../../pages/ide/custom-pom/theia-vsc';
 import { PreferenceIds, TheiaPreferenceView } from '../../pages/ide/theia-pom/theia-preference-view';
 
-test.describe('Theia Artemis Integration - Scorpio', () => {
+test.skip('Theia Artemis Integration - Scorpio', () => {
     test.describe.configure({ mode: 'serial' });
 
     test.use({
